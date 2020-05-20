87033ed57d3bb85214f3dccb251f5f8d92bc60db
Soumeh <stefi.brezovac@gmail.com>
2020-05-20 09:31:43 +0200

    0.10

--- font.js
+++ font.js
@@ -1,8 +1,8 @@
-View rootView = inflater.inflate(R.layout.fragment1, container, false);
-mWebView = (WebView) rootView.findViewById(R.id.enter_text);
+WebView web = (WebView)findViewById(R.id.webby);
+//For avoiding a weird error message
+web.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 
-//Font must be placed in assets/fonts folder
-String text = "<html><style type='text/css'>@font-face { font-family: Minecraftia; src: url('fonts/Minecraftia.ttf'); } body p {font-family: Minecraftia;}</style>" 
-+ "<body >" + "<p align=\"justify\" style=\"font-size: 22px; font-family: Minecraftia;\">" + getString(R.string.enter_text) + "</p> "+ "</body></html>";
-mWebView.loadDataWithBaseURL("file:///android_asset/",text,"text/html","utf-8",null);
-return rootView;
+String webContent = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><link rel=\"stylesheet\" href=\"stylesheet.css\"></head>"
+                            + "<body><img src='image.png' width=\"100px\"><div class=\"running\">I am a text rendered with INDIGO</div></body></html>";
+String internalFilePath = "file://" + getFilesDir().getAbsolutePath() + "/";
+web.loadDataWithBaseURL(internalFilePath, webContent, "text/html", "UTF-8", "");
